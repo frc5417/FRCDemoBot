8f4300c4e76c257af6fd593b18f5f6d711fec4c6
bkapla01 <bkapla01@penguin>
2022-08-23 16:55:13 -0500

    first commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.*;
+import frc.robot.subsystems.*;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  private final static Joystick joystick = new Joystick(0);
+
+  // The robot's subsystems and commands are defined here...
+  private final static Drivetrain m_drivetrainSubsystem = new Drivetrain(joystick);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {}
+  
+  public Drivetrain getDrivetrain() {
+    return m_drivetrainSubsystem;
+  }
+
+}
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+
+public class Drivetrain extends SubsystemBase {
+  private static Joystick joystick;
+
+  private final static Spark leftMotor1 = new Spark(Constants.leftMotor1Port);
+  private final static Spark leftMotor2 = new Spark(Constants.leftMotor2Port);
+  private final static Spark rightMotor1 = new Spark(Constants.rightMotor1Port);
+  private final static Spark rightMotor2 = new Spark(Constants.rightMotor2Port);
+
+  /** Creates a new Drivetrain. */
+  public Drivetrain(Joystick stick) {
+    leftMotor1.setInverted(true);
+    leftMotor2.setInverted(true);
+
+    joystick = stick;
+  }
+
+  public void setPower(double leftPower, double rightPower) {
+    // sets the speed for the left and right motors
+    leftMotor1.set(leftPower);
+    // leftMotor2.set(leftPower);
+    
+    // rightMotor1.set(rightPower);
+    // rightMotor2.set(rightPower);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    setPower(joystick.getRawAxis(1) * Constants.speedMultiplier, joystick.getRawAxis(5) * Constants.speedMultiplier);
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
